96d59866cb04e72c299deb2908f0d912fb75a6e0
wolf <billodida420@gmail.com>
2022-04-18 14:08:01 +0300

    adding tests

--- AirdropPlatform/hardhat.config.ts
+++ AirdropPlatform/hardhat.config.ts
@@ -1,21 +1,21 @@
-require("@nomiclabs/hardhat-waffle");
-require('dotenv').config();
-require('@nomiclabs/hardhat-ethers');
-require("@nomiclabs/hardhat-etherscan");
+import "@nomiclabs/hardhat-waffle";
+import 'dotenv';
+import '@nomiclabs/hardhat-ethers';
+import "@nomiclabs/hardhat-etherscan";
 
 const { API_URL, PRIVATE_KEY, ETHERSCAN_KEY} = process.env;
 
  module.exports = {
-  defaultNetwork: "ropsten",
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
     },
-    ropsten: {
-      url: API_URL,
-      accounts: [PRIVATE_KEY],
-      gas: 5000000,
-      gasPrice: 15000000000,
-    }
+    // ropsten: {
+    //   url: API_URL,
+    //   accounts: [PRIVATE_KEY],
+    //   gas: 5000000,
+    //   gasPrice: 15000000000,
+    // }
   },
   solidity: {
     version: "0.8.3",
---
+++ AirdropPlatform/test/utils.ts
@@ -0,0 +1,13 @@
+export enum AirDropType {
+    LOTTERY,
+    USER_LIMITED,
+    TOKEN_LIMITED
+}
+
+export enum AirDropRequirement {
+    TOKEN_REQUIRED,
+    NFT_REQUIRED,
+    PASSWORD
+
+}
+
